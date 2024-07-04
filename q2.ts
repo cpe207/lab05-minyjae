8cd4bdad32055aa3c1aa0e1bf6552436b8acffbd
minyjae <jiradate_ora@cmu.ac.th>
2024-07-04 20:21:05 +0700

    finish jing jing

--- q2.ts
+++ q2.ts
@@ -1,9 +1,13 @@
 // define interface for Student object
-/* Your code here */
+interface Student {
+  name : string;
+  score : number;
+}
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
-  /* Your code here */
+function findTopNames(students : Student[]){
+    let top = students.filter((students) => students.score > 8).map((students) => students.name);
+    return top;
 }
 
 // assign interface/type to the student1 object properly
